158f419d4812bcc184b22fed4635e59e82a31477
Sheldoff <alik28061996@gmail.com>
2022-04-28 22:54:16 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+app.use(express.static(__dirname + '/assets'))
+
+io.on('connection', (socket) => {
+    socket.on('chat message', (data) => {
+        io.emit('chat message', {
+            message: data.message,
+            name: data.name
+        })
+    })
+})
+
+
+http.listen(3000, () => {
+    console.log('started server')
+})
+
+// 0 создать github (как работает vscode and github)
+// 1 устaновить Mongodb
+// 2 создать базу данных
+// 3 создать таблицу messages
+// 4 on submit => save in to ''messages''
+// 5 после авторизации скачать с баз даннх все сообщения и отобразить их (io.emit)
+// 6 улучшить интерфейс
